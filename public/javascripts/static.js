cd2e05f501a45fe58c167438d8e4e8a9fed3672e
Pedritto <pedritto@pedritto-3000-N100.(none)>
2014-04-14 21:44:27 +0200

    add more static content

--- public/javascripts/static.js
+++ public/javascripts/static.js
@@ -1,3 +1,13 @@
 loadStatic = function() {
+	var elements = ['#menu'];
+	//var playFiles = ['stylesheets/MenuItems.html'];
+	var staticFiles = ['includes/menuItems.html'];	
+	
+	var files = staticFiles;
+	
+	
+	for(var i = 0; i < elements.length; i++) {
+		$(elements[i]).load(files[i]);
+	}
     alert('loading...');
 }
